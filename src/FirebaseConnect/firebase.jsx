91f37053eef362a59b474af9a3f0dd1d5e11fd6d
noury05 <nouryounis5804@gmail.com>
2025-05-28 15:01:36 +0300

    first commit

--- src/FirebaseConnect/firebase.jsx
+++ src/FirebaseConnect/firebase.jsx
@@ -5,14 +5,14 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD-W0y0rmjdBCb_ZizylvcfR9hoZgxq68c",
-    authDomain: "skillexchange-77b7d.firebaseapp.com",
-    databaseURL: "https://skillexchange-77b7d-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "skillexchange-77b7d",
-    storageBucket: "skillexchange-77b7d.firebasestorage.app",
-    messagingSenderId: "1070505142813",
-    appId: "1:1070505142813:web:7a6c064b647fcb76a8b4f4",
-    measurementId: "G-7NLXKG3TLT"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 
